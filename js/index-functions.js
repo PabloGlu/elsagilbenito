c679aa14620dbad332991c63e8187519ef7d6cf4
Pablo da Cunha <pablodacun@gmail.com>
2023-06-28 18:03:55 +0200

    h

---
+++ js/index-functions.js
@@ -0,0 +1,11 @@
+document.getElementById("indexhomeButton").onclick = function () {
+    location.href = "index.html";
+    };
+
+document.getElementById("indexportfolioButton").onclick = function () {
+    location.href = "html/portfolio.html";
+    };
+
+document.getElementById("indexcontactButton").onclick = function () {
+    location.href = "html/contact.html";
+    };   
